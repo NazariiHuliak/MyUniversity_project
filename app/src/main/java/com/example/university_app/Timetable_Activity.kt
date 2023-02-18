cff6c89632f0e61853a2497b9ea4f051b99dfa7d
Danylo77 <dankoiniesta@gmail.com>
2023-02-18 13:57:06 +0200

    created Timetable UI )

---
+++ app/src/main/java/com/example/university_app/Timetable_Activity.kt
@@ -0,0 +1,11 @@
+package com.example.university_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Timetable_Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_timetable)
+    }
+}
