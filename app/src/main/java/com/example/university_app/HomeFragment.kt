981aa90399bb3aa9110e989a41707d672d1c5005
AndriyK12 <124818943+AndriyK12@users.noreply.github.com>
2023-02-21 15:57:12 +0200

    fixed popUpWindow bug on Home Fragment

--- app/src/main/java/com/example/university_app/HomeFragment.kt
+++ app/src/main/java/com/example/university_app/HomeFragment.kt
@@ -1,37 +1,53 @@
 package com.example.university_app
 
-import android.R
-import android.R.attr.button
 import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.WindowManager
+import android.widget.ImageButton
+import android.widget.LinearLayout
 import android.widget.PopupWindow
-import androidx.core.content.ContextCompat.getSystemService
 import androidx.fragment.app.Fragment
-
+import com.example.university_app.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var myView: View
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-//        val root_view = inflater.inflate(R.layout.fragment_home, container, false)
-//        val btn_contacts = root_view.findViewById<ImageButton>(R.id.profile_photo)
-//        btn_contacts.setOnClickListener {
-//            val dialogBining = layoutInflater.inflate(R.layout.account_dialog, null)
-//
-//            val context = requireActivity()
-//            val account_dialog = Dialog(context)
-//            account_dialog.setContentView(dialogBining)
-//
-//            account_dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-//            account_dialog.show()
-//        }
-//        return root_view
-        return inflater.inflate(com.example.university_app.R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        myView = view.findViewById<View>(R.id.popup_background)
+
+        fun showPopupWindow(view: View) {
+            val inflater = requireContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            val popupView = inflater.inflate(R.layout.account_dialog, null)
+
+            val width = LinearLayout.LayoutParams.WRAP_CONTENT
+            val height = LinearLayout.LayoutParams.WRAP_CONTENT
+            val focusable = true // lets taps outside the popup also dismiss it
+            val popupWindow = PopupWindow(popupView, width, height, focusable)
+            popupWindow.setOnDismissListener {
+                myView.visibility = View.INVISIBLE
+            }
+
+            popupWindow.showAsDropDown(view)
+        }
+
+        val button = view.findViewById<ImageButton>(R.id.profile_photo)
+        button.setOnClickListener {
+            myView.visibility = View.VISIBLE
+            showPopupWindow(button)
+        }
     }
 }
--- app/src/main/java/com/example/university_app/MainActivity.kt
+++ app/src/main/java/com/example/university_app/MainActivity.kt
@@ -31,41 +31,40 @@ class MainActivity : AppCompatActivity() {
         navController = Navigation.findNavController(this, R.id.activiti_main_nav_host_fragment)
         setupWithNavController(binding.bottomNavigationView, navController)
 
-        /*val myButton = findViewById<ImageButton>(R.id.profile_photo)*/
-        val myView = findViewById<View>(R.id.popup_background)
-
-        /*myButton.setOnClickListener {
-            myView.visibility = View.VISIBLE
-        }*/
-        // inflate the layout of the popup window
-        fun showPopupWindow(view: View) {
-            // inflate the layout of the popup window
-            val inflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
-            val popupView = inflater.inflate(R.layout.account_dialog, null)
-
-            // create the popup window
-            val width = LinearLayout.LayoutParams.WRAP_CONTENT
-            val height = LinearLayout.LayoutParams.WRAP_CONTENT
-            val focusable = true // lets taps outside the popup also dismiss it
-            val popupWindow = PopupWindow(popupView, width, height, focusable)
-            popupWindow.setOnDismissListener {
-                myView.visibility = View.INVISIBLE
-            }
-/*            popupWindow.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-            popupWindow.isOutsideTouchable = true
-            popupWindow.isFocusable = true
-            popupWindow.setBackgroundDrawable(ColorDrawable(Color.parseColor("#80000000"))) // Set the alpha to 80%
-            popupWindow.update()*/
-
-            // show the popup window
-            popupWindow.showAsDropDown(view)
-        }
-        val button = findViewById<ImageButton>(R.id.profile_photo)
-        button.setOnClickListener {
-            myView.visibility = View.VISIBLE
-            showPopupWindow(button)
-        }
-
+//        /*val myButton = findViewById<ImageButton>(R.id.profile_photo)*/
+//        val myView = findViewById<View>(R.id.popup_background)
+//
+//        /*myButton.setOnClickListener {
+//            myView.visibility = View.VISIBLE
+//        }*/
+//        // inflate the layout of the popup window
+//        fun showPopupWindow(view: View) {
+//            // inflate the layout of the popup window
+//            val inflater = getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+//            val popupView = inflater.inflate(R.layout.account_dialog, null)
+//
+//            // create the popup window
+//            val width = LinearLayout.LayoutParams.WRAP_CONTENT
+//            val height = LinearLayout.LayoutParams.WRAP_CONTENT
+//            val focusable = true // lets taps outside the popup also dismiss it
+//            val popupWindow = PopupWindow(popupView, width, height, focusable)
+//            popupWindow.setOnDismissListener {
+//                myView.visibility = View.INVISIBLE
+//            }
+///*            popupWindow.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+//            popupWindow.isOutsideTouchable = true
+//            popupWindow.isFocusable = true
+//            popupWindow.setBackgroundDrawable(ColorDrawable(Color.parseColor("#80000000"))) // Set the alpha to 80%
+//            popupWindow.update()*/
+//
+//            // show the popup window
+//            popupWindow.showAsDropDown(view)
+//        }
+//        val button = findViewById<ImageButton>(R.id.profile_photo)
+//        button.setOnClickListener {
+//            myView.visibility = View.VISIBLE
+//            showPopupWindow(button)
+//        }
         /*val area = findViewById<androidx.constraintlayout.widget.ConstraintLayout>(R.id.app_area)
         area.SetOnTouchListener{
 
