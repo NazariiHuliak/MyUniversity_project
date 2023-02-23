f9cb11746c5c31faf7462a67f444ef8ee2b36377
Orik25 <orestpilavka@gmail.com>
2023-02-23 23:17:52 +0200

    added audience_UI

--- app/src/main/java/com/example/university_app/AuditoryFragment.kt
+++ app/src/main/java/com/example/university_app/AuditoryFragment.kt
@@ -5,13 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class AuditoryFragment : Fragment() {
+
+    private lateinit var newRecyclerView: RecyclerView
+    private lateinit var newArraylist : ArrayList<Audiences>
+    lateinit var imageid : Array<Int>
+    lateinit var heading : Array<String>
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_auditory, container, false)
+        val root_view = inflater.inflate(R.layout.fragment_auditory, container, false)
+        imageid = arrayOf(R.drawable.lock,R.drawable.lock,R.drawable.lock,R.drawable.lock)
+        heading = arrayOf("413","413","413","413")
+        newRecyclerView = root_view.findViewById(R.id.recycler)
+        newRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        newRecyclerView.setHasFixedSize(true)
+
+        newArraylist = arrayListOf<Audiences>()
+        getData()
+        return root_view
     }
+    private fun getData(){
+        for(i in imageid.indices){
+            val rooms = Audiences(imageid[i],heading[i])
+            newArraylist.add(rooms)
+        }
+        newRecyclerView.adapter = Adapter_aud(newArraylist)
+    }
+
 }
