c03fa6f2d269ac68515ae5b821fe8b85071c08e9
AndriyK12 <124818943+AndriyK12@users.noreply.github.com>
2023-02-26 18:05:59 +0200

    added validation of data and messages about errors/successful inputs

--- app/src/main/java/com/example/university_app/Register_Activity.kt
+++ app/src/main/java/com/example/university_app/Register_Activity.kt
@@ -2,13 +2,21 @@ package com.example.university_app
 
 import android.content.ContentValues
 import android.content.Context
+import android.content.Intent
 import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
 import android.widget.Button
+import android.widget.Toast
 import com.google.android.material.textfield.TextInputEditText
+import java.util.*
+import javax.mail.*
+import javax.mail.internet.InternetAddress
+import javax.mail.internet.MimeMessage
 
 class Register_Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,12 +36,100 @@ class Register_Activity : AppCompatActivity() {
             val group = groupEditText.text.toString()
             val password = passwordEditText.text.toString()
 
-            dbAdapter.insertData(username,email,group,password)
-            // обрабка даних бд
+
+            if(!isValidUsername(username)){
+                usernameEditText.error = "Ім'я користувача не може бути порожнім"
+                usernameEditText.requestFocus()
+            }
+            else if (!isEmailValid(email)) {
+                emailEditText.error = "Введіть коректну адресу електронної пошти!"
+                emailEditText.requestFocus()
+            }
+            else if (dbAdapter.isEmailExists(email)) {
+                emailEditText.error = "Аккаунт з цією поштою вже існує!!"
+                emailEditText.requestFocus()
+            }
+            else if (!isValidGroup(group)){
+                groupEditText.error = "Введіть коректну групу!"
+                groupEditText.requestFocus()
+            }
+            else if (!isValidPassword(password)){
+                passwordEditText.error = "Пароль не може бути порожнім, та мусить містити щонайменше 8 символів!"
+                passwordEditText.requestFocus()
+            }
+            else {
+                Toast.makeText(this@Register_Activity, "Реєстрація пройшла успішно!", Toast.LENGTH_SHORT).show()
+                dbAdapter.insertData(username,email,group,password)
+//                val intent = Intent(this, MainActivity::class.java)
+//                startActivity(intent)
+            }
+
+            // обробка даних бд
         }
     }
     val dbAdapter = MyDbAdapter(this)
+
+
+    fun isValidUsername(inputString: String): Boolean {
+        return inputString.isNotBlank()
+    }
+    fun isValidGroup(input: String): Boolean {
+        val pattern = Regex("[A-ZА-ЯІЇЄ]{3}-\\d{2}")
+        return pattern.matches(input)
+    }
+    private fun isEmailValid(email: String): Boolean {
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+    fun isValidPassword(inputString: String): Boolean {
+        return inputString.isNotBlank() && inputString.length >= 8
+    }
+    //    fun emailResponder(result: Boolean){
+//        if (result) {
+//            Toast.makeText(this@Register_Activity, "Реєстрація успішна!", Toast.LENGTH_SHORT).show()
+//        } else {
+//            Toast.makeText(this@Register_Activity, "Помилка! Введено неправильну пошту", Toast.LENGTH_SHORT).show()
+//        }
+//    }
 }
+//    private inner class SendEmailTask(val email: String) : AsyncTask<Void, Void, Boolean>() {
+//
+//        override fun doInBackground(vararg params: Void?): Boolean {
+//            val props = Properties()
+//            props.put("mail.smtp.auth", "true")
+//            props.put("mail.smtp.starttls.enable", "true")
+//            props.put("mail.smtp.host", "smtp.gmail.com")
+//            props.put("mail.smtp.port", "587")
+//
+//            val session = Session.getInstance(props, object : Authenticator() {
+//                override fun getPasswordAuthentication(): PasswordAuthentication {
+//                    return PasswordAuthentication("myuniversity240@gmail.com", "myunivpass")
+//                }
+//            })
+//
+//            try {
+//                val message = MimeMessage(session)
+//                message.setFrom(InternetAddress("myuniversity24@proton.me"))
+//                message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(email))
+//                message.setSubject("Вітання від MyUniversity!")
+//                message.setText("Привіт! Це автоматично згенерований лист, що підтверджує вашу реєстрацію у додатку MyUniversity.")
+//
+//                Transport.send(message)
+//
+//                return true
+//            } catch (e: MessagingException) {
+//                e.printStackTrace()
+//                return false
+//            }
+//        }
+//        override fun onPostExecute(result: Boolean) {
+//            if (result) {
+//                Toast.makeText(this@Register_Activity, "Реєстрація успішна! На введену пошту надіслано лист з підтвердженням", Toast.LENGTH_SHORT).show()
+//            } else {
+//                Toast.makeText(this@Register_Activity, "Помилка! Введено неправильну пошту", Toast.LENGTH_SHORT).show()
+//            }
+//        }
+//    }
+//}
 class DatabaseHelper(context: Context?) :
     SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
     override fun onCreate(db: SQLiteDatabase) {
@@ -87,10 +183,21 @@ class MyDbAdapter(private val context: Context) {
 //        val whereArgs = arrayOf(id.toString())
 //        return database.update("UsersInfo", values, whereClause, whereArgs)
 //    }
-
-    fun deleteData(id: Long): Int {
-        val whereClause = "id=?"
-        val whereArgs = arrayOf(id.toString())
-        return database.delete("UsersInfo", whereClause, whereArgs)
+fun deleteData(id: Long): Int {
+    val whereClause = "id=?"
+    val whereArgs = arrayOf(id.toString())
+    return database.delete("UsersInfo", whereClause, whereArgs)
+}
+    fun clearData() {
+        database.execSQL("DELETE FROM UsersInfo")
+    }
+    fun isEmailExists(email: String): Boolean {
+        val columns = arrayOf("email")
+        val selection = "email = ?"
+        val selectionArgs = arrayOf(email)
+        val cursor = database.query("UsersInfo", columns, selection, selectionArgs, null, null, null)
+        val exists = cursor.count > 0
+        cursor.close()
+        return exists
     }
 }
