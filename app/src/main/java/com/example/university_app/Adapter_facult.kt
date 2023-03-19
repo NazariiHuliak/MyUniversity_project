3beac5a470d774265792b0646a0d051d165886bf
Orik25 <orestpilavka@gmail.com>
2023-03-19 12:23:04 +0200

    implementatoin info

---
+++ app/src/main/java/com/example/university_app/Adapter_facult.kt
@@ -0,0 +1,70 @@
+package com.example.university_app
+
+
+import android.app.Dialog
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat.startActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.imageview.ShapeableImageView
+
+class Adapter_facult(private val dataList: MutableList<FacultiesModel>,private val dialog:Dialog?,private val main_view: View?) :
+    RecyclerView.Adapter<Adapter_facult.View_Holder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): View_Holder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.list_item_facult,parent,false)
+        return View_Holder(itemView)
+    }
+
+    override fun getItemCount(): Int {
+        return dataList.size
+    }
+
+    override fun onBindViewHolder(holder: View_Holder, position: Int) {
+        val currentItem = dataList[position]
+        holder.icon.setImageResource(currentItem.logoID)
+        holder.name.text = currentItem.name
+
+
+    }
+
+    inner class View_Holder(itemView : View) : RecyclerView.ViewHolder(itemView), View.OnClickListener{
+        val icon : ShapeableImageView = itemView.findViewById(R.id.facul_icon)
+        val name : TextView = itemView.findViewById(R.id.facult_name)
+        init {
+            itemView.setOnClickListener(this)
+        }
+        override fun onClick(view: View) {
+            dialog?.dismiss()
+            main_view?.findViewById<ImageView>(R.id.info_logo)?.setImageResource(dataList[adapterPosition].logoID.toInt())
+            main_view?.findViewById<TextView>(R.id.info_name)?.setText("Назва: "+dataList[adapterPosition].name.toString())
+            main_view?.findViewById<TextView>(R.id.info_location)?.setText("Адреса: "+dataList[adapterPosition].adress.toString())
+            main_view?.findViewById<TextView>(R.id.info_phone)?.setText("Телефон: "+dataList[adapterPosition].phone.toString())
+            main_view?.findViewById<TextView>(R.id.info_email)?.setText("Пошта: "+dataList[adapterPosition].email.toString())
+            main_view?.findViewById<TextView>(R.id.info_site)?.setText("Сайт: "+dataList[adapterPosition].site.toString())
+            main_view?.findViewById<TextView>(R.id.info_site)?.setOnClickListener {
+                val url = "https://"+dataList[adapterPosition].site.toString()+"/"
+                val intent = Intent(Intent.ACTION_VIEW)
+                intent.data = Uri.parse(url)
+                it.context.startActivity(intent)
+            }
+            val map = main_view?.findViewById<ImageButton>(R.id.map_button)
+//            map?.setImageResource(dataList[adapterPosition].mapID.toInt())
+            map?.setBackgroundResource(dataList[adapterPosition].mapID.toInt())
+            map?.setOnClickListener {
+                val url = dataList[adapterPosition].mapURL.toString()
+                val intent = Intent(Intent.ACTION_VIEW)
+                intent.data = Uri.parse(url)
+                it.context.startActivity(intent)
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/university_app/FacultiesDataBaseHelper.kt
@@ -0,0 +1,60 @@
+package com.example.university_app
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import com.readystatesoftware.sqliteasset.SQLiteAssetHelper
+
+class FacultiesDataBaseHelper(context: Context) : SQLiteAssetHelper(context, DATABASE_NAME, null, DATABASE_VERSION){
+    companion object {
+        private const val DATABASE_NAME = "faculties.db"
+        private const val DATABASE_VERSION = 1
+    }
+}
+class DatabaseAccessFacult private constructor(context: Context) {
+    private val openHelper: SQLiteOpenHelper = FacultiesDataBaseHelper(context)
+    private lateinit var database: SQLiteDatabase
+
+    companion object {
+        private var instance: DatabaseAccessFacult? = null
+
+        @Synchronized
+        fun getInstance(context: Context): DatabaseAccessFacult {
+            if (instance == null) {
+                instance = DatabaseAccessFacult(context)
+            }
+            return instance!!
+        }
+    }
+    fun open() {
+        database = openHelper.writableDatabase
+    }
+    fun close() {
+        database.close()
+    }
+
+    @SuppressLint("Range")
+    fun getAllData(): MutableList<FacultiesModel> {
+        var dataList: MutableList<FacultiesModel> = mutableListOf()
+        val cursor: Cursor = database.rawQuery("SELECT * FROM faculties", null)
+
+        if (cursor.moveToFirst()) {
+            do {
+                val id: Int = cursor.getInt(cursor.getColumnIndex("id"))
+                val name: String = cursor.getString(cursor.getColumnIndex("name"))
+                val adress: String = cursor.getString(cursor.getColumnIndex("adress"))
+                val phone: String = cursor.getString(cursor.getColumnIndex("phone"))
+                val email: String = cursor.getString(cursor.getColumnIndex("email"))
+                val site: String = cursor.getString(cursor.getColumnIndex("site"))
+                val logoID: Int = cursor.getInt(cursor.getColumnIndex("logoID"))
+                val mapID: Int = cursor.getInt(cursor.getColumnIndex("mapID"))
+                val url: String = cursor.getString(cursor.getColumnIndex("mapURL"))
+                dataList.add(FacultiesModel(id, name, adress, phone, email, site,logoID,mapID,url))
+            } while (cursor.moveToNext())
+        }
+        cursor.close()
+        return dataList
+    }
+}
